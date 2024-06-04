063e7c973582b86eef0bb22ecf03e539a43d21b6
unknown <afrizal.aminulloh@gmail.com>
2024-06-04 14:58:01 +0700

    firts commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: new NotesService(),
+    },
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
